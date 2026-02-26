ac536e55d82cb7ff9c8ec815dcebb7b4160c3bbc
pouya <pouyakhaleghi82@gmail.com>
2026-02-27 03:21:31 +0330

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import { Toaster } from 'react-hot-toast';
+import './App.css'
+import Header from "./Components/Header.jsx";
+import LocationList from "./Components/LocationList.jsx";
+import {Route , Routes} from "react-router-dom";
+
+function App() {
+
+  return (
+    <div className="App">
+        <Toaster />
+        <Header />
+        <Routes>
+            <Route path="/" element={<LocationList />} />
+            <Route path="/map" element={<Map />} />
+        </Routes>
+    </div>
+  )
+
+}
+
+
+
+
+
+export default App
---
+++ src/Components/IsLoading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const IsLoading = () => {
+    return (
+        <div>
+            <h1>loading....</h1>
+        </div>
+    );
+};
+
+export default IsLoading;
+
